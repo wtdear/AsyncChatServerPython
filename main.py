59728155012a5bd3a67e83607f2f55ccf7e063b3
Johhny Kolbaya <den.kolbaya@yandex.ru>
2026-01-14 18:40:15 +0300

    fisrt steps

--- main.py
+++ main.py
@@ -1,53 +1,21 @@
 import asyncio
+import logging
+import websockets
+import json
+import os
 
+# LOGGING
 
-clients = set()
+os.makedirs("logs", exist_ok=True)
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logs/log.log",
+    filemode="w",
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
-async def handle_client(reader, writer):
-    clients.add(writer)
-    addr = writer.get_extra_info('peername')
-    print(f"Подключён {addr}")
+# SERVER
 
-
-    try:
-        while True:
-            data = await reader.read(1024)
-            if not data:
-                break
-
-
-        message = data.decode()
-        print(f"{addr}: {message}")
-
-
-        for client in clients:
-            if client != writer:
-                client.write(data)
-                await client.drain()
-
-
-    except Exception as e:
-        print("Ошибка:", e)
-
-
-    finally:
-        clients.remove(writer)
-        writer.close()
-        await writer.wait_closed()
-        print(f"Отключён {addr}")
-
-
-async def main():
-    server = await asyncio.start_server(
-    handle_client,
-    '0.0.0.0',
-    8888
-    )
-
-
-    async with server:
-        await server.serve_forever()
-
-
-asyncio.run(main())
+connected_clients = {}
